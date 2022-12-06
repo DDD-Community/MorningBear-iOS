4ca730b1d1ac3c0b49a4f2f6b6fbc9d9fe06aee2
Young Bin Lee <enebin777@gmail.com>
2022-12-06 14:56:34 +0900

    ✨:  Add Alertable wrapper for Error

---
+++ Targets/MorningBearUI/Sources/Alert/Extensions/Error + Extension.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  MorningBearUI
+//
+//  Created by 이영빈 on 2022/12/06.
+//  Copyright © 2022 com.dache. All rights reserved.
+//
+
+import UIKit
+
+extension Error {
+    // Help turning Error into Alertable
+    var alertify: some Alertable {
+        return ErrorAlert(self)
+    }
+}
+
+fileprivate struct ErrorAlert<T>: Alertable where T: Error {
+    private let error: T
+    let alertComponent: AlertComponent
+    
+    init(_ error: T) {
+        self.error = error
+        self.alertComponent = AlertComponent(title: "에러 발생",
+                                             message: "\(error.localizedDescription)",
+                                             buttons: [UIAlertAction(title: "OK", style: .default)])
+    }
+}
