67360321b9b1effa2e3705f5f73e27e4a05208e0
2dubu <2dubu.dev@gmail.com>
2022-12-08 00:22:23 +0900

    :sparkles: : Add SceneDelegate

--- Targets/MorningBear/Sources/AppDelegate.swift
+++ Targets/MorningBear/Sources/AppDelegate.swift
@@ -7,8 +7,6 @@ import FirebaseCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
@@ -20,11 +18,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let viewController = UIViewController()
         viewController.view.backgroundColor = .white
         
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
-        
-        
         return true
     }
+    
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
 
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {
+        
+    }
 }
---
+++ Targets/MorningBear/Sources/SceneDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  SceneDelegate.swift
+//  MorningBear
+//
+//  Created by 이건우 on 2022/12/07.
+//  Copyright © 2022 com.dache. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .white
+        
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+    }
+}
