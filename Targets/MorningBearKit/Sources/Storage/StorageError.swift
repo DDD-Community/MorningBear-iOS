a6ff63c959f6728c492007db51e08d71bc0ce51e
Young Bin Lee <enebin777@gmail.com>
2022-12-08 14:41:27 +0900

    ✨: Add error case

--- Targets/MorningBearKit/Sources/Storage/StorageError.swift
+++ Targets/MorningBearKit/Sources/Storage/StorageError.swift
@@ -11,8 +11,10 @@ import Foundation
 enum StorageError: LocalizedError {
     case invalidData
     case invalidImage
+    case invalidPath
     case failToLoadImage
     case failToSaveImage
+    case failToFetchData
     
     var errorDescription: String? {
         switch self {
@@ -20,10 +22,14 @@ enum StorageError: LocalizedError {
             return "데이터를 불러올 수 없습니다"
         case .invalidImage:
             return "이미지 처리 중 오류가 발생했습니다"
+        case .invalidPath:
+            return "경로가 잘못되었습니다"
         case .failToLoadImage:
             return "이미지 다운로드 중 오류가 발생했습니다"
         case .failToSaveImage:
             return "이미지 업로드 중 오류가 발생했습니다"
+        case .failToFetchData:
+            return "저장된 데이터를 불러올 수 없습니다"
         }
     }
 }
