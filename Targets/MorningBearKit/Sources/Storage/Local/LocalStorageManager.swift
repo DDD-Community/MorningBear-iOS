89a566a4732eb0fa17d745f4d989b2f9ae4cec30
Young Bin Lee <enebin777@gmail.com>
2022-12-08 14:41:55 +0900

    ✨: Add local storage management - With FileManager

---
+++ Targets/MorningBearKit/Sources/Storage/Local/LocalStorageManager.swift
@@ -0,0 +1,47 @@
+//
+//  StorageManager.swift
+//  MorningBearKit
+//
+//  Created by 이영빈 on 2022/12/08.
+//  Copyright © 2022 com.dache. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+
+public struct LocalStorageManager<Instance, Storager> where Instance: Codable, Storager: StoragerType {
+    private let localStorager: Storager
+    private let coderSet: CoderSet
+    
+    public func save(_ instance: Instance, name: String) -> Single<URL> {
+        guard let data = try? coderSet.encoder.encode(instance) else {
+            return Single.error(StorageError.invalidImage)
+        }
+        
+        return localStorager.save(data: data, name: name)
+    }
+    
+    public func load(path: URL) -> Single<Instance> {
+        let downloadTask = localStorager.download(with: path)
+            .map { data in
+                guard let instance = try? coderSet.decoder.decode(Instance.self, from: data) else {
+                    throw StorageError.invalidData
+                }
+                
+                return instance
+            }
+        
+        return downloadTask
+    }
+    
+    public init(_ localStorager: Storager = LocalStorager()) {
+        self.localStorager = localStorager
+        self.coderSet = CoderSet()
+    }
+}
+
+fileprivate struct CoderSet {
+    let encoder = JSONEncoder()
+    let decoder = JSONDecoder()
+}
---
+++ Targets/MorningBearKit/Sources/Storage/Local/LocalStorager.swift
@@ -0,0 +1,74 @@
+//
+//  LocalStorager.swift
+//  MorningBearKit
+//
+//  Created by 이영빈 on 2022/12/08.
+//  Copyright © 2022 com.dache. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+struct LocalStorager: StoragerType {
+    private let storage: FileManager
+    
+    init(_ storage: FileManager = FileManager.default) {
+        self.storage = storage
+    }
+
+    func save(data: Data, name: String?) -> Single<URL> {
+        let singleTrait = Single<URL>.create { observer in
+            do {
+                let filePath = try fileURL(with: name)
+                try data.write(to: filePath)
+                
+                observer(.success(filePath))
+            } catch let error {
+                observer(.failure(error))
+            }
+            
+            return Disposables.create()
+        }
+        
+        return singleTrait
+    }
+    
+    func download(with url: URL) -> Single<Data> {
+        let singleTrait = Single<Data>.create { observer in
+            do {
+                guard storage.fileExists(atPath: url.relativeString) else {
+                    throw StorageError.invalidPath
+                }
+                
+                let data = try Data(contentsOf: url)
+                observer(.success(data))
+            } catch let error {
+                observer(.failure(error))
+            }
+            
+            return Disposables.create()
+        }
+        
+        return singleTrait
+    }
+}
+
+extension LocalStorager {
+    var documentDirectory: URL? {
+        return try? self.storage.url(for: .documentDirectory,
+                                     in: .userDomainMask,
+                                     appropriateFor: nil,
+                                     create: false)
+    }
+    
+    func fileURL(with name: String?) throws -> URL {
+        guard let documentDirectory = documentDirectory else {
+            throw StorageError.failToFetchData
+        }
+        
+        let parsedName = name ?? UUID().uuidString
+        
+        return documentDirectory.appendingPathComponent(parsedName, conformingTo: .data)
+    }
+}
