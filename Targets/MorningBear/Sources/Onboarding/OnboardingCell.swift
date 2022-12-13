6a437ee888e762afe025f9c74baa496e25de9663
2dubu <2dubu.dev@gmail.com>
2022-12-14 02:57:38 +0900

    :sparkles: : Add onboarding cell

---
+++ Targets/MorningBear/Sources/Onboarding/OnboardingCell.swift
@@ -0,0 +1,25 @@
+//
+//  OnboardingCell.swift
+//  MorningBear
+//
+//  Created by 이건우 on 2022/12/14.
+//  Copyright © 2022 com.dache. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(
+        indexPath: IndexPath,
+        onboardingData: [OnboardingData]
+    ) {
+        imageView.image = UIImage(named: onboardingData[indexPath.row].image)
+        titleLabel.text = onboardingData[indexPath.row].title
+        descriptionLabel.text = onboardingData[indexPath.row].description
+    }
+}
+
