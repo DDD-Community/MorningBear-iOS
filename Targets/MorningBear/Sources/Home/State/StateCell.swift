2d2bd126f0dd5321b8a5c6d783552efb8989a0cc
Young Bin Lee <enebin777@gmail.com>
2022-12-26 21:21:59 +0900

    ✨: Add basic config for state section - Topmost part of the home view

---
+++ Targets/MorningBear/Sources/Home/State/StateCell.swift
@@ -0,0 +1,18 @@
+//
+//  StateViewCell.swift
+//  MorningBear
+//
+//  Created by 이영빈 on 2022/12/26.
+//  Copyright © 2022 com.dache. All rights reserved.
+//
+
+import UIKit
+
+class StateCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
