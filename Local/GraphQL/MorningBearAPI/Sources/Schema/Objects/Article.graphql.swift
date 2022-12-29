988bbc48af1aec671c194ae3485ba14f30df3e1b
enebin <enebin777@gmail.com>
2022-12-29 16:55:40 +0900

    ✨: Add article query

---
+++ Local/GraphQL/MorningBearAPI/Sources/Schema/Objects/Article.graphql.swift
@@ -0,0 +1,12 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public extension Objects {
+  ///  아티클 정보 
+  static let Article = Object(
+    typename: "Article",
+    implementedInterfaces: []
+  )
+}
--- Local/GraphQL/MorningBearAPI/TestMocks/Query+Mock.graphql.swift
+++ Local/GraphQL/MorningBearAPI/TestMocks/Query+Mock.graphql.swift
@@ -12,16 +12,19 @@ public class Query: MockObject {
   public struct MockFields {
     @Field<[Badge?]>("findAllBadge") public var findAllBadge
     @Field<[Login?]>("findLoginInfo") public var findLoginInfo
+    @Field<[Article?]>("searchArticle") public var searchArticle
   }
 }
 
 public extension Mock where O == Query {
   convenience init(
     findAllBadge: [Mock<Badge>?]? = nil,
-    findLoginInfo: [Mock<Login>?]? = nil
+    findLoginInfo: [Mock<Login>?]? = nil,
+    searchArticle: [Mock<Article>?]? = nil
   ) {
     self.init()
     self.findAllBadge = findAllBadge
     self.findLoginInfo = findLoginInfo
+    self.searchArticle = searchArticle
   }
 }
