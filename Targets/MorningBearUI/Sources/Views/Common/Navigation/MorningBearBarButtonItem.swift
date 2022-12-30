0e00f529fe70d04af785617380980e77ae824980
Young Bin Lee <enebin777@gmail.com>
2022-12-30 22:46:32 +0900

    ✨: Add bar button item module

---
+++ Targets/MorningBearUI/Sources/Views/Common/Navigation/MorningBearBarButtonItem.swift
@@ -0,0 +1,15 @@
+//
+//  MorningBearBarButtonItem.swift
+//  MorningBear
+//
+//  Created by 이영빈 on 2022/12/30.
+//  Copyright © 2022 com.dache. All rights reserved.
+//
+
+import UIKit
+
+struct MorningBearBarButtonItem {
+    static let titleButton =  UIBarButtonItem(title: "모닝베어~")
+    static let searchButton = UIBarButtonItem(systemItem: .search)
+    static let notificationButton = UIBarButtonItem(systemItem: .rewind)
+}
