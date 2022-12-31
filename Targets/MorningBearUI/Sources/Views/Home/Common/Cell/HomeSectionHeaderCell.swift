d37d4d24e6d27bcb094f3ccc5965d419ddcdf09a
Young Bin Lee <enebin777@gmail.com>
2022-12-31 13:57:11 +0900

    ✨: Connect RxCocoa to header button

--- Targets/MorningBearUI/Sources/Views/Home/Common/Cell/HomeSectionHeaderCell.swift
+++ Targets/MorningBearUI/Sources/Views/Home/Common/Cell/HomeSectionHeaderCell.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 public class HomeSectionHeaderCell: UICollectionViewCell {
     public typealias Action = () -> Void
     
+    // View components
     @IBOutlet weak var stackView: UIStackView! {
         didSet {
             stackView.distribution = .equalSpacing
@@ -27,17 +31,25 @@ public class HomeSectionHeaderCell: UICollectionViewCell {
             moreButton.titleLabel?.font = MorningBearUIFontFamily.Pretendard.regular.font(size: 12)
         }
     }
+    
+    // Internal variables
+    private let bag = DisposeBag() // For RxCocoa
+    
+    /// 버튼 표시를 결정
     private var needsButton = false {
         didSet {
             // 버튼이 필요하면 숨기지 말 것
             moreButton.isHidden = !needsButton
         }
     }
+    
+    /// 버튼 액션; 기본값으로 아무것도 하지 않음
     private var buttonAction: Action = {}
     
+    
+    // Methods
     public override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
     
     public override func prepareForReuse() {
@@ -46,6 +58,7 @@ public class HomeSectionHeaderCell: UICollectionViewCell {
     }
     
     public func prepare(descText: String?, titleText: String?, buttonAction: Action? = nil) {
+        // 액션이 없으면 버튼은 표시되지 않음
         if let buttonAction {
             self.needsButton = true
             self.buttonAction = buttonAction
@@ -54,8 +67,23 @@ public class HomeSectionHeaderCell: UICollectionViewCell {
             self.buttonAction = {}
         }
         
-
         self.titleLabel.text = titleText
         self.moreButton.setTitle(descText, for: .normal)
+        
+        bindButton()
+    }
+}
+
+// MARK: Interanal tools
+extension HomeSectionHeaderCell {
+    private func bindButton() {
+        self.moreButton.rx.tap.bind { [weak self] in
+            guard let self = self else {
+                return
+            }
+            
+            self.buttonAction()
+        }
+        .disposed(by: bag)
     }
 }
