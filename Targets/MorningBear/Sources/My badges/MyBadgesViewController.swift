67bf210d42d4dbbd42e1c8258eb41a774f0ba7b9
Young Bin Lee <enebin777@gmail.com>
2022-12-31 21:13:46 +0900

    ✨: Add my badges view controller

---
+++ Targets/MorningBear/Sources/My badges/MyBadgesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MyBadgesViewController.swift
+//  MorningBear
+//
+//  Created by 이영빈 on 2022/12/31.
+//  Copyright © 2022 com.dache. All rights reserved.
+//
+
+import UIKit
+
+class MyBadgesViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
