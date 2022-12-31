3533171071b94d684f89c74e5ec425da666e8888
Young Bin Lee <enebin777@gmail.com>
2022-12-31 15:20:42 +0900

    📝 : Add comments .

--- Targets/MorningBearUI/Tests/Home/Cell/HomeSectionHeaderCellTests.swift
+++ Targets/MorningBearUI/Tests/Home/Cell/HomeSectionHeaderCellTests.swift
@@ -35,6 +35,7 @@ final class HomeSectionHeaderCellTests: XCTestCase {
     }
     
     func test__prepare_only_title() {
+        // 타이틀 외에 버튼에 관한 정보를 입력하지 않으면 버튼이 비활성화 되는 것이 기본 로직임
         let titleText = "title_text"
         
         XCTAssertNotEqual(header.titleLabel.text, titleText)
@@ -61,6 +62,6 @@ final class HomeSectionHeaderCellTests: XCTestCase {
         
         XCTAssertEqual(header.titleLabel.text, titleText)
         XCTAssertEqual(header.moreButton.title(for: .normal), buttonText)
-        XCTAssertFalse(header.isButtonDisabled)
+        XCTAssertFalse(header.isButtonDisabled) // 버튼이 활성화 되었는지 확인해야 함
     }
 }
