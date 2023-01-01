e7384ff8624bf026ce04679d278641243c952960
Young Bin Lee <enebin777@gmail.com>
2023-01-01 20:35:26 +0900

    ✨: Add my badge view header cell

---
+++ Targets/MorningBearUI/Sources/Views/My badges/MyBadgeState.swift
@@ -0,0 +1,12 @@
+//
+//  MyBadgeState.swift
+//  MorningBearUI
+//
+//  Created by 이영빈 on 2023/01/01.
+//  Copyright © 2023 com.dache. All rights reserved.
+//
+
+public struct MyBadgeState {
+    let nickname: String
+    let badgeCount: Int
+}
