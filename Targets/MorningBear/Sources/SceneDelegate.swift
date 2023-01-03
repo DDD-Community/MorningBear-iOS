e079367225db77905487b6e23a3fe84951235f8f
Young Bin Lee <enebin777@gmail.com>
2023-01-03 17:29:31 +0900

    ♻️ :  Encapsulate navigation configuration codes

--- Targets/MorningBear/Sources/SceneDelegate.swift
+++ Targets/MorningBear/Sources/SceneDelegate.swift
@@ -27,27 +27,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window = window
         
 //        let viewController = UIStoryboard(name: "Onboarding", bundle: nil).instantiateViewController(withIdentifier: "OnboardingVC")
-        // FIXME: .
+//        let viewController = UIStoryboard(name: "Login", bundle: nil).instantiateViewController(withIdentifier: "Login")
         let mainViewController = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "Home")
-        let navigationController = UINavigationController(rootViewController: mainViewController)
-        let viewController = UIStoryboard(name: "Login", bundle: nil).instantiateViewController(withIdentifier: "Login")
+        let navigationController = UINavigationController(
+            rootViewController: mainViewController
+        ).configureMorningBearDefaultNavigationController()
         
-        // Design navigation controller
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = MorningBearUIAsset.Colors.gray100.color
-        
-        appearance.titleTextAttributes = [
-            .font: MorningBearUIFontFamily.Pretendard.bold.font(size: 20)
-        ]
-        
-        let backButton = MorningBearUIAsset.Asset.backArrow.image.withTintColor(.black, renderingMode: .alwaysOriginal)
-        appearance.setBackIndicatorImage(backButton, transitionMaskImage: backButton)
-
-        navigationController.navigationBar.standardAppearance = appearance
-        navigationController.navigationBar.topItem?.backButtonTitle = ""
-//        navigationController.hidesBarsOnSwipe = true
-
         // Set root view
         window.rootViewController = navigationController // 루트 뷰컨트롤러 생성
         window.makeKeyAndVisible()
@@ -59,5 +44,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 _ = AuthController.rx.handleOpenUrl(url: url)
             }
         }
+    }
+}
+
+private extension UINavigationController {
+    func configureMorningBearDefaultNavigationController() -> UINavigationController {
+        // Design navigation controller
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = MorningBearUIAsset.Colors.gray100.color
+        
+        // 네비게이션 타이틀 텍스트 설정
+        appearance.titleTextAttributes = [
+            .font: MorningBearUIFontFamily.Pretendard.bold.font(size: 20)
+        ]
+        
+        let backButton = MorningBearUIAsset.Asset.backArrow.image.withTintColor(.black, renderingMode: .alwaysOriginal)
+        appearance.setBackIndicatorImage(backButton, transitionMaskImage: backButton)
+
+        self.navigationBar.standardAppearance = appearance
+        self.navigationBar.topItem?.backButtonTitle = "" // 안 하면 뒤로가기 버튼에 글자 생김
+        
+        // TODO: TBD
+//        self.hidesBarsOnSwipe = true
+
+        return self
     }
 }
