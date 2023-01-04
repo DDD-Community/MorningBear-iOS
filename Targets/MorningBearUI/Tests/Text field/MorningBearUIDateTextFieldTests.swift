7296e614276f8bdb3d58f89c219f15aec74e951c
enebin <enebin777@gmail.com>
2023-01-05 08:18:49 +0900

    🧪: Add test file

---
+++ Targets/MorningBearUI/Tests/Text field/MorningBearUIDateTextFieldTests.swift
@@ -0,0 +1,26 @@
+//
+//  MorningBearUIDateTextFieldTests.swift
+//  MorningBearUITests
+//
+//  Created by Young Bin on 2023/01/04.
+//  Copyright © 2023 com.dache. All rights reserved.
+//
+
+import XCTest
+@testable import MorningBearUI
+
+final class MorningBearUIDateTextFieldTests: XCTestCase {
+    private var dateTextField: MorningBearUIDateTextField!
+
+    override func setUpWithError() throws {
+        dateTextField = MorningBearUIDateTextField()
+    }
+
+    override func tearDownWithError() throws {
+        dateTextField = nil
+    }
+
+    func test__done_button() throws {
+        dateTextField.
+    }
+}
