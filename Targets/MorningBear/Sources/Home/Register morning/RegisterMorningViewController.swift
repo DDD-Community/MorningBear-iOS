dde73ac10b668208e2ecf7b19b201c4a40387d39
Young Bin Lee <enebin777@gmail.com>
2023-01-04 15:18:19 +0900

    ✨ : Add register morning view

---
+++ Targets/MorningBear/Sources/Home/Register morning/RegisterMorningViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterMorningViewController.swift
+//  MorningBear
+//
+//  Created by 이영빈 on 2023/01/04.
+//  Copyright © 2023 com.dache. All rights reserved.
+//
+
+import UIKit
+
+class RegisterMorningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
