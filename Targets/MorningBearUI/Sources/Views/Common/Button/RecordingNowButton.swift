9b6b95ef63a1cb38cebda93ba48ac0a24434b748
enebin <enebin777@gmail.com>
2023-01-11 22:26:36 +0900

    :recycle: : Refactor recording button

---
+++ Targets/MorningBearUI/Sources/Views/Common/Button/RecordingNowButton.swift
@@ -0,0 +1,74 @@
+//
+//  RecordingNowButton.swift
+//  MorningBearUI
+//
+//  Created by Young Bin on 2023/01/11.
+//  Copyright © 2023 com.dache. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+public class RecordingNowButton: UIView {
+    public typealias Action = () -> Void
+
+    private let bag = DisposeBag()
+    private var buttonAction: Action?
+    
+    @IBOutlet var wrapperView: UIView! {
+        didSet {
+            wrapperView.backgroundColor = .clear
+            self.backgroundColor = MorningBearUIAsset.Colors.gray800.color
+            self.layer.cornerRadius = 8
+        }
+    }
+    @IBOutlet weak var timeLabel: UILabel! {
+        didSet {
+            timeLabel.font = MorningBearUIFontFamily.Pretendard.bold.font(size: 16)
+            timeLabel.textColor = .white
+        }
+    }
+    @IBOutlet weak var stopButton: UIButton! {
+        didSet {
+            stopButton.setTitle("종료하기", for: .normal)
+            stopButton.setTitleColor(.white, for: .normal)
+            stopButton.titleLabel?.font = MorningBearUIFontFamily.Pretendard.regular.font(size: 12)
+            
+            stopButton.backgroundColor = MorningBearUIAsset.Colors.canceledDark.color
+            stopButton.layer.cornerRadius = 8
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        loadXib()
+        bindButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        loadXib()
+        bindButton()
+    }
+}
+
+private extension RecordingNowButton {
+    func bindButton() {
+        stopButton.rx.tap.bind { [weak self] in
+            guard let self else { return }
+            
+            self.buttonAction?()
+        }
+        .disposed(by: bag)
+    }
+}
+
+public extension RecordingNowButton {
+    func prepare(action: @escaping Action) {
+        buttonAction = action
+    }
+}
