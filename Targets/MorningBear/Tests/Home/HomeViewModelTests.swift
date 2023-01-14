cd7a45fdf7ca5c0dd3a7e1a48e28ced40b812a5c
Young Bin Lee <enebin777@gmail.com>
2023-01-14 14:24:50 +0900

    :sparkles: : Add dependencies quick, nimble, mockingbird .

--- Targets/MorningBear/Tests/Home/HomeViewModelTests.swift
+++ Targets/MorningBear/Tests/Home/HomeViewModelTests.swift
@@ -13,22 +13,20 @@ final class HomeViewModelTests: XCTestCase {
     var viewModel: HomeViewModel!
 
     override func setUpWithError() throws {
-        viewModel = HomeViewModel()
+        let mockHomeDataProvider = HomeViewDataProvider(UserDefaults(suiteName: "test home")!)
+        viewModel = HomeViewModel(mockHomeDataProvider)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test__start_recording() throws {
+        viewModel.startRecording()
     }
 }
 
 private extension HomeViewModelTests {
+    
     
 }
