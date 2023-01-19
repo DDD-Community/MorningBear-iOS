ee67f6edfc1f866adf34b472381ac3e708915b80
2dubu <2dubu.dev@gmail.com>
2023-01-20 01:05:05 +0900

    :sparkles: : Add UIPageViewController extension

---
+++ Targets/MorningBearUI/Sources/Utils/UIPageViewController + Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIPageViewController + Extension.swift
+//  MorningBearUI
+//
+//  Created by 이건우 on 2023/01/20.
+//  Copyright © 2023 com.dache. All rights reserved.
+//
+
+import Foundation
