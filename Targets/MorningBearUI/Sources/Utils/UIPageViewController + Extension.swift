5d3015d7bdb752816dc8f7d1e6c44aaaa4a3b985
2dubu <2dubu.dev@gmail.com>
2023-01-20 01:07:50 +0900

    :sparkles: : Add UIPageViewController extension

--- Targets/MorningBearUI/Sources/Utils/UIPageViewController + Extension.swift
+++ Targets/MorningBearUI/Sources/Utils/UIPageViewController + Extension.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2023 com.dache. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIPageViewController {
+
+    public func enableSwipeGesture() {
+        for view in self.view.subviews {
+            if let subView = view as? UIScrollView {
+                subView.isScrollEnabled = true
+            }
+        }
+    }
+
+    public func disableSwipeGesture() {
+        for view in self.view.subviews {
+            if let subView = view as? UIScrollView {
+                subView.isScrollEnabled = false
+            }
+        }
+    }
+}
