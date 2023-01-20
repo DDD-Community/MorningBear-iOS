9dc09560ef6d47c7397a9b34001cffd89a7f0685
2dubu <2dubu.dev@gmail.com>
2023-01-20 19:29:38 +0900

    :sparkles: : Add input viewcontrollers

---
+++ Targets/MorningBear/Sources/Login/InputViews/SetProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SetProfileViewController.swift
+//  MorningBear
+//
+//  Created by 이건우 on 2023/01/20.
+//  Copyright © 2023 com.dache. All rights reserved.
+//
+
+import UIKit
+import MorningBearUI
+
+import RxSwift
+import RxCocoa
+
+class SetProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
