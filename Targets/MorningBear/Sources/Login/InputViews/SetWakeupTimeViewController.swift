710193b66b7ded2fd582aa643295122d698b7fdd
2dubu <2dubu.dev@gmail.com>
2023-01-24 01:08:51 +0900

    :sparkles: : SetWakeupTimeVC

--- Targets/MorningBear/Sources/Login/InputViews/SetWakeupTimeViewController.swift
+++ Targets/MorningBear/Sources/Login/InputViews/SetWakeupTimeViewController.swift
@@ -14,6 +14,8 @@ import RxCocoa
 
 class SetWakeupTimeViewController: UIViewController {
     
+    private let bag = DisposeBag()
+    
     @IBOutlet weak var titleLabel: UILabel! {
         didSet {
             titleLabel.font = MorningBearUIFontFamily.Pretendard.bold.font(size: 24)
@@ -28,8 +30,38 @@ class SetWakeupTimeViewController: UIViewController {
             descriptionLabel.text = "모너가 대신 깨워드릴게요"
         }
     }
+    @IBOutlet weak var timePickerPopUpButton: UIButton! {
+        didSet {
+            timePickerPopUpButton.layer.cornerRadius = 8
+            timePickerPopUpButton.layer.borderColor = MorningBearUIAsset.Colors.gray700.color.cgColor
+            timePickerPopUpButton.layer.borderWidth = 1
+        }
+    }
+    @IBOutlet weak var timeLabel: UILabel! {
+        didSet {
+            timeLabel.font = MorningBearUIFontFamily.Pretendard.regular.font(size: 16)
+            timeLabel.textColor = MorningBearUIAsset.Colors.gray700.color
+            timeLabel.text = "오전  5 : 00"
+        }
+    }
+    @IBOutlet weak var downArrowImage: UIImageView! {
+        didSet {
+            downArrowImage.image = MorningBearUIAsset.Images.arrowDecrease.image
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        bindButton()
+    }
+    
+    private func bindButton() {
+        timePickerPopUpButton.rx.tap
+            .withUnretained(self)
+            .bind { owner, _ in
+               // TODO: picker view with half modal
+            }
+            .disposed(by: bag)
     }
 }
