05f18d1fb970cce3d60ba3171c54bcc0ae0b5872
enebin <enebin777@gmail.com>
2023-01-29 15:19:16 +0900

    :sparkles: : Connect refresh control foundation

--- Targets/MorningBearKit/Sources/Rx/LinkRx.swift
+++ Targets/MorningBearKit/Sources/Rx/LinkRx.swift
@@ -9,17 +9,50 @@
 import RxSwift
 
 /// ViewModel에서 `subscribe` 작업 반복되는 것 캡슐화
-public func linkRx<Value>(_ singleTrait: Single<Value>,
-                    scheduler: SchedulerType = ConcurrentDispatchQueueScheduler(qos: .userInitiated),
-                    in bag: DisposeBag,
-                    completionHandler: @escaping (Value) -> Void) {
+public func linkRx<Value>(
+    _ singleTrait: Single<Value>,
+    scheduler: SchedulerType = ConcurrentDispatchQueueScheduler(qos: .userInitiated),
+    in bag: DisposeBag,
+    completionHandler: @escaping (Value) -> Void,
+    errorHandler: ((Error) -> Void)? = nil,
+    disposeHandler: (() -> Void)? = nil
+) {
     singleTrait.subscribe(on: scheduler)
         .subscribe(
             onSuccess: { data in
                 completionHandler(data)
             },
             onFailure: {
+                errorHandler?($0)
                 MorningBearLogger.track($0)
+            },
+            onDisposed: {
+                disposeHandler?()
+            }
+        )
+        .disposed(by: bag)
+}
+
+/// ViewModel에서 `subscribe` 작업 반복되는 것 캡슐화
+public func linkDriver<Value>(
+    _ singleTrait: Single<Value>,
+    scheduler: SchedulerType = ConcurrentDispatchQueueScheduler(qos: .userInitiated),
+    in bag: DisposeBag,
+    completionHandler: @escaping (Value) -> Void,
+    errorHandler: ((Error) -> Void)? = nil,
+    disposeHandler: (() -> Void)? = nil
+) {
+    singleTrait.subscribe(on: scheduler)
+        .subscribe(
+            onSuccess: { data in
+                completionHandler(data)
+            },
+            onFailure: {
+                errorHandler?($0)
+                MorningBearLogger.track($0)
+            },
+            onDisposed: {
+                disposeHandler?()
             }
         )
         .disposed(by: bag)
---
+++ Targets/MorningBearKit/Sources/Rx/NetworkFlag.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkFlag.swift
+//  MorningBearKit
+//
+//  Created by Young Bin on 2023/01/29.
+//  Copyright © 2023 com.dache. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+struct NetworkFlag {
+    var isNetworking: Bool = false
+    
+}
