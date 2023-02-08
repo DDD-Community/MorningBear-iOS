78b7531696a1ae0c31d979c4077b35304375fab4
2dubu <2dubu.dev@gmail.com>
2023-02-08 12:49:32 +0900

    :sparkles: : Add Button feature

--- Targets/MorningBear/Sources/Onboarding/OnboardingViewModel.swift
+++ Targets/MorningBear/Sources/Onboarding/OnboardingViewModel.swift
@@ -9,10 +9,20 @@
 import MorningBearUI
 import MorningBearData
 
+import RxSwift
+import RxRelay
+
 class OnboardingViewModel {
-    let onboardingData: [OnboardingData] = [
-        OnboardingData(image: MorningBearUIAsset.Images.onboarding1.image, title: "미라클모닝 후 인증해요", description: "기상을 인증하고 내가 하고\n미라클 모닝을 기록해요"),
-        OnboardingData(image: MorningBearUIAsset.Images.onboarding2.image, title: "동료들을 응원해요", description: "다른 미라클 모닝러들을 만나고\n응원하며 소통해요"),
-        OnboardingData(image: MorningBearUIAsset.Images.onboarding3.image, title: "뱃지들을 모아봐요", description: "뱃지들을 모으며 나의 미라클 모닝 활동을\n한눈에 확인해요")
-    ]
+    
+    var currentIndex: BehaviorRelay<Int>
+    let onboardingData: [OnboardingData]
+    
+    init() {
+        self.currentIndex = BehaviorRelay(value: 0)
+        self.onboardingData = [
+            OnboardingData(image: MorningBearUIAsset.Images.onboarding1.image, title: "미라클모닝 후 인증해요", description: "기상을 인증하고 내가 하고\n미라클 모닝을 기록해요"),
+            OnboardingData(image: MorningBearUIAsset.Images.onboarding2.image, title: "동료들을 응원해요", description: "다른 미라클 모닝러들을 만나고\n응원하며 소통해요"),
+            OnboardingData(image: MorningBearUIAsset.Images.onboarding3.image, title: "뱃지들을 모아봐요", description: "뱃지들을 모으며 나의 미라클 모닝 활동을\n한눈에 확인해요")
+        ]
+    }
 }
